79283f5cf392cc5e3b1a9c701104127a0a54ade7
Ben Hodgson <ben.hodgson@lendable.co.uk>
2025-08-08 18:04:02 +0100

    Fix repro structure

---
+++ zudoku.config.tsx
@@ -0,0 +1,36 @@
+import type { ZudokuConfig } from "zudoku";
+
+const config: ZudokuConfig = {
+  navigation: [
+    { type: "link", label: "Users API", to: "/users-api-spec" },
+    { type: "link", label: "Orders API", to: "/orders-api-spec" },
+    { type: "link", label: "Products API", to: "/products-api-spec" },
+  ],
+  catalogs: {
+    path: "/api-specs",
+    label: "API Specs",
+    items: ["users-api-spec", "orders-api-spec", "products-api-spec"],
+  },
+  apis: [
+    {
+      type: "file",
+      input: "./specs/users-api-spec/src/index.yml",
+      path: "/users-api-spec",
+      categories: [{ label: "General", tags: ["Users"] }],
+    },
+    {
+      type: "file",
+      input: "./specs/orders-api-spec/src/index.yml",
+      path: "/orders-api-spec",
+      categories: [{ label: "General", tags: ["Orders"] }],
+    },
+    {
+      type: "file",
+      input: "./specs/products-api-spec/src/index.yml",
+      path: "/products-api-spec",
+      categories: [{ label: "General", tags: ["Products"] }],
+    },
+  ],
+};
+
+export default config;
